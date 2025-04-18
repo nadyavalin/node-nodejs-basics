aeabb5ac2b01e7083c16025f8fdcf0c2d09dd0ba
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-18 15:44:11 +0300

    feat: implement function that prints all array of filenames from files folder into console - work with list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from "fs/promises";
+import path from "path";
+
+const folderPath = path.join("src", "fs", "files");
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access(folderPath);
+    const files = await fs.readdir(folderPath);
+    console.log(files);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await list();
