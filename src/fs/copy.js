ea8e75f21f7fc78d032ff732536b50a5975601a5
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-18 15:11:00 +0300

    feat: implement function that copies folder files files with all its content into folder files_copy - work with copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+import path from "path";
+
+const sourcePath = path.join('src', 'fs', 'files');
+const destinationPath = path.join('src', 'fs', 'files_copy');
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.access(sourcePath);
+    try {
+      await fs.access(destinationPath);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+    await fs.cp(sourcePath, destinationPath, { recursive: true });
+  } catch (error) {
+    if (error.code === 'ENOENT' || error.message === 'FS operation failed') {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await copy();
