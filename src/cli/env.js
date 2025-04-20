482e9ae3a60bc2b489c754d9f0baa938998687bb
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-20 16:08:48 +0300

    feat: implement function that parses environment variables with prefix RSS_ - work with env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`);
+  console.log(envVars.join("; "));
 };
 
 parseEnv();
