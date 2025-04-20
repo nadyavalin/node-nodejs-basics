46ae74297926d31b5fbe3f0056c4fb1e872dc27b
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-20 16:26:18 +0300

    feat: implement function that parses command line arguments and prints them to the console - work with args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const pairs = [];
+  for (let i = 0; i < args.length; i += 2) {
+    pairs.push([args[i], args[i + 1]]);
+  }
+  const formattedArgs = pairs.map(([propName, value]) => {
+    return `${propName.replace(/^--/, "")} is ${value}`;
+  });
+  console.log(formattedArgs.join(", "));
 };
 
 parseArgs();
