f59d52bbef9e87e70b0b2ce0762e518878daea0f
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-21 22:25:54 +0300

    feat: add console.log that tells you what to do when write.js runs

--- src/streams/write.js
+++ src/streams/write.js
@@ -8,6 +8,9 @@ const sourcePath = join(__dirname, "files");
 const filePath = join(sourcePath, "fileToWrite.txt");
 
 const write = async () => {
+  console.log(
+    "Write something you want to write to the file 'fileToWrite.txt' and press Enter:"
+  );
   const stream = createWriteStream(filePath);
   await pipeline(process.stdin, stream);
 };
