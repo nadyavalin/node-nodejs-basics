4df87e7296ac6344cbdb8d6cabe3cf76db726260
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-21 22:36:57 +0300

    feat: implement function that decompresses archive.gz back to the fileToCompress.txt with same content - work with decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourcePath = join(__dirname, "files");
+const inputFilePath = join(sourcePath, "archive.gz");
+const outputFilePath = join(sourcePath, "fileToCompress.txt");
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream(inputFilePath);
+  const gunzipStream = createGunzip();
+  const writeStream = createWriteStream(outputFilePath);
+  await pipeline(readStream, gunzipStream, writeStream);
 };
 
 await decompress();
