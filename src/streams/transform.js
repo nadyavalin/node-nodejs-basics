d59f0052f4909a2ab59b25271616117791583128
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-21 22:21:47 +0300

    feat: implement function that reads data from process.stdin and reverses text - work with transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
+const transformStream = new Transform({
+  transform(chunk, _, callback) {
+    const reversed = chunk.toString().split("").reverse().join("");
+    callback(null, reversed);
+  },
+});
+
 const transform = async () => {
-    // Write your code here 
+  console.log("Write the line you want to reverse:");
+  await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
