18dc4cd50ef97e4646f3248441c5c86f534b5648
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-21 22:15:17 +0300

    feat: implement function that writes process.stdin data into file - work with write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { pipeline } from "stream/promises";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourcePath = join(__dirname, "files");
+const filePath = join(sourcePath, "fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const stream = createWriteStream(filePath);
+  await pipeline(process.stdin, stream);
 };
 
 await write();
