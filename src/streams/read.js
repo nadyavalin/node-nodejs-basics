8e25e7c393fa0d7d7cd154ed7e4670d702def2d3
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2025-04-21 18:20:10 +0300

    feat: implement function that reads file - work with read.js in streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { once } from "events";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const sourcePath = join(__dirname, "files");
+const filePath = join(sourcePath, "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const stream = createReadStream(filePath);
+  stream.pipe(process.stdout);
+  await once(stream, "end");
+  console.log();
 };
 
 await read();
